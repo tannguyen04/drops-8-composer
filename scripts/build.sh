de3240dae9df98962e5204f0c553c0f5bdfdba25
LC <chuyen.luu@ffwagency.com>
2017-05-29 15:22:04 +0700

    fix file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-# Variables
+JSON=composer.json
+EXE=composer
 
+# CI options
+export COMPOSER_DISCARD_CHANGES=1
+export COMPOSER_NO_INTERACTION=1
 
-echo -e "\nBuild"
+if [ ! -f "$JSON" ]
+then
+  echo ${txtred}Error: No $JSON found ${txtrst}
+  exit 1
+fi
+
+FOUND=`which $EXE`
+if [ ! -x "$FOUND" ]
+then
+  echo ${txtred}Error: executable composer not found on path ${txtrst}
+  exit 1
+fi
+
+COMPOSER_PARMS="--no-ansi --no-dev --no-interaction --optimize-autoloader --no-progress --no-scripts"
+
+# Install Composer dependencies
+echo -e "\n${txtylw}Invoking: $FOUND install $COMPOSER_PARMS ${txtrst}"
+$FOUND install $COMPOSER_PARMS
+
